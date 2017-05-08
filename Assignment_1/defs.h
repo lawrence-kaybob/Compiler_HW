cc1c8f924148a0f645e1c238b12d8ee3dfef16b2
Lawernce Kay <lkay9495@hotmail.com>
2017-05-08 11:44:05 +0900

    Assignment 1 Commiteed

---
+++ Assignment_1/defs.h
@@ -0,0 +1,66 @@
+#define IDENTIFIER	1
+#define INTEGER 	2
+#define FLOATPOINT	3
+#define STRING		4
+#define COMMENT		5
+#define SEMICOLON	6
+#define COLON		7
+#define WHITESPACE	8
+#define DOUBLEQUOTATION 9
+#define SINGLECHARACTER 10
+#define COMMA	11
+#define DIGIT 12
+#define QUOTATION 13
+
+#define LEFTPARANTHESIS 20
+#define RIGHTPARANTHESIS 21
+#define LEFTBRACE 22
+#define RIGHTBRACE 23
+#define LEFTBRACKET 24
+#define RIGHTBRACKET 25
+
+#define INCLUDEPROCESSOR 26
+#define STANDARDHEADER 27
+#define DEFINEPROCESSOR 28
+
+#define INT 		40
+#define CHAR 		41
+#define DOUBLE 		42
+#define VOID 		43
+#define IF 			44
+#define ELSE 		45
+#define FOR 		46
+#define WHILE		47
+#define RETURN 		48
+
+#define PLUS		50
+#define MINUS		51
+#define MULTIPLY	52
+#define DIVIDE		53
+#define MODULO		54
+#define NEGATION	55
+#define BINARYAND	56
+#define BINARYOR	57
+
+#define PLUSONE		60
+#define MINUSONE	61
+
+#define LESS		70
+#define GREATER		71
+#define ASSIGN		72
+#define LESSEQUAL	73
+#define GREATEREQUAL 74
+#define EQUAL		75
+#define NOTEQUAL	76
+
+#define ASSIGNPLUS 80
+#define ASSIGNMINUS 81
+#define ASSIGNMULT	82
+#define ASSIGNDIV	83
+#define ASSIGNMODULO 84
+
+#define LOGICAND	90
+#define LOGICOR		91
+
+#define COMMENTPREFIX 92
+#define COMMENTPOSTFIX 93
